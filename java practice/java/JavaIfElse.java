5d90333cab8796be1e99d877bee1c10d3198a9d0
Vimalesh28 <95900470+Vimalesh28@users.noreply.github.com>
2022-12-29 21:27:29 +0530

    Create JavaIfElse.java

---
+++ java practice/java/JavaIfElse.java
@@ -0,0 +1,11 @@
+int N = scanner.nextInt();
+        if(N%2!=0 || (N>=6 && N<=20)){
+            System.out.print("Weird");
+        }
+        else{
+             System.out.print("Not Weird");
+        }
+    }
+}
+
+https://www.hackerrank.com/challenges/java-if-else/problem
