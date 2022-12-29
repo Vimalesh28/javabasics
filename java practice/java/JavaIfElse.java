b64f10517ba98382d2ca312a3bf3c62f083b8359
Vimalesh28 <95900470+Vimalesh28@users.noreply.github.com>
2022-12-29 21:29:55 +0530

    Update JavaIfElse.java

--- java practice/java/JavaIfElse.java
+++ java practice/java/JavaIfElse.java
@@ -1,4 +1,19 @@
-int N = scanner.nextInt();
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
         if(N%2!=0 || (N>=6 && N<=20)){
             System.out.print("Weird");
         }
@@ -7,5 +22,6 @@ int N = scanner.nextInt();
         }
     }
 }
+
 
 https://www.hackerrank.com/challenges/java-if-else/problem
